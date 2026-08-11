7bd56983dd7a6474ef917803aaeeac515d22ac3f
Caleb Baraka <caleb.blessings@student.moringaschool.com>
2026-08-11 12:49:00 +0300

    linked components to appjsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import blogData from "../data/blog";
 
+import Header from "./header";
+import About from "./about";
+import Articlelist from "./articlelist";
+
 console.log(blogData);
 
 function App() {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-    </div>
+  <div>
+    <Header />
+    <About />
+    <Articlelist />
+  </div>
   );
 }
 
